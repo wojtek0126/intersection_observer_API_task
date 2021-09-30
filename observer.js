450550f513fe261262863814c322007f36941cca
wojtek0126 <41778298+wojtek0126@users.noreply.github.com>
2021-09-30 14:22:19 +0200

    split to sections with classes added

---
+++ observer.js
@@ -0,0 +1,5 @@
+if ('IntersectionObserver' in window) {
+    console.log("Your browser supports IntersectionObserver");
+} else {
+    console.log("Your browser does not support IntersectionObserver");
+}

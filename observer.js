788d5d5aff807770e788855633b892bf84acb01e
wojtek0126 <41778298+wojtek0126@users.noreply.github.com>
2021-09-30 19:03:41 +0200

    intersection effect on scroll done

--- observer.js
+++ observer.js
@@ -1,5 +1,34 @@
-if ('IntersectionObserver' in window) {
-    console.log("Your browser supports IntersectionObserver");
-} else {
-    console.log("Your browser does not support IntersectionObserver");
-}
+const initialColor = 'rgba(255, 255, 0, 0)';
+const highlightColor = 'rgba(255, 255, 0, 1)'; 
+
+const targets = 'section';
+
+let observerOptions = {
+    root: null,
+    rootMargin: '120px 0px 0px 0px',
+    threshold: 0.1
+};
+
+window.onscroll = function () { 
+    const observer = new IntersectionObserver(observerCallback, observerOptions);
+    function observerCallback(entries) {
+        entries.forEach((entry) => {
+           
+                if(entry.isIntersecting) {
+            
+                    entry.target.style.backgroundColor = highlightColor;
+                  }
+                  else{
+                      entry.target.style.backgroundColor = initialColor; 
+                  }                 
+        });    
+    };
+    document.querySelectorAll(targets).forEach(element => {    
+        observer.observe(element);   
+});
+}   
+
+
+
+
+

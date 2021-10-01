ac27c42a45b99814d136d29ccc0496e42d00cd8c
wojtek0126 <41778298+wojtek0126@users.noreply.github.com>
2021-10-01 13:51:27 +0200

    no more onscroll

--- observer.js
+++ observer.js
@@ -1,31 +1,47 @@
+
+const targetSections = document.querySelectorAll('section');
+const firstSection = document.getElementById('first-section');
+
 const initialColor = 'rgba(255, 255, 0, 0)';
 const highlightColor = 'rgba(255, 255, 0, 1)'; 
 
-const targets = document.querySelectorAll('section');
+let triggerBool = false;
 
 const observerOptions = {
     root: null,
-    rootMargin: '-120px 0px 0px 0px',
-    threshold: 0.1
+    rootMargin: '110px',
+    threshold: 0.2
 };
 
-window.onscroll = function () { 
-    const observer = new IntersectionObserver(observerCallback, observerOptions);
-    function observerCallback(entries) {
-        entries.forEach((entry) => {           
-                if(entry.isIntersecting) {            
-                    entry.target.style.backgroundColor = highlightColor;
-                  }
-                  else{
-                      entry.target.style.backgroundColor = initialColor; 
-                  };                 
-        });    
-    };
-        
-    targets.forEach(element => {           
-        observer.observe(element);   
+// first observer to watch for scroll move and initiate second observer when true
+const scrollMoveObserver = new IntersectionObserver(entries => {
+    console.log(entries);
+    if (entries[0].boundingClientRect.y < 0) {
+    triggerBool = true;
+    firstSection.style.backgroundColor = highlightColor;
+        }
     });
-};   
+
+scrollMoveObserver.observe(document.querySelector("#pixel-to-watch"));
+
+// second ovserver to initiate background color change
+const observer = new IntersectionObserver(observerCallback, observerOptions);
+
+function observerCallback(entries) {
+        entries.forEach((entry) => {     
+            if(entry.isIntersecting && triggerBool === true) {            
+                entry.target.style.backgroundColor = highlightColor;
+              }
+            else{
+                entry.target.style.backgroundColor = initialColor; 
+            };                                       
+    })
+};  
+
+targetSections.forEach(element => {           
+    observer.observe(element);   
+});
+ 
 
 
 
